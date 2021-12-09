0c2326a6e198b7ae5386e2e416fddbc478092160
ptylczynski <piotr@ptl.cloud>
2021-12-10 00:19:20 +0100

    change: use generated layout

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print('hello')
+import sys
+
+from PyQt5 import QtWidgets
+from qt_material import apply_stylesheet
+
+from ui.py.main import Ui_mainWindow
+
+
+class MyApp(QtWidgets.QMainWindow, Ui_mainWindow):
+    def __init__(self, parent=None):
+        super(MyApp, self).__init__(parent)
+        # Load the login form
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.c)
+
+    def c(self):
+        print('sdfsd')
 
 
 if __name__ == '__main__':
-    main()
+    app = QtWidgets.QApplication(sys.argv)
+    apply_stylesheet(app, theme='dark_amber.xml', invert_secondary=False)
+    # Create class object
+    form = MyApp()
+    # Display the form
+    form.show()
+    # Start the event loop of the app or dialog box
+    app.exec()
